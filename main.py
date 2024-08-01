1e2ef0e81e2c71bfa6ea72406641bc9f9097b986
wolf <arthurwolf.dev07@gmail.com>
2024-07-31 23:37:11 -0600

    UPDATE finish functions

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# DEPENDENCIES
 from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel, Field, validator
 from typing import Annotated
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-# SELF PROJECT IMPORTS
 import models
 from connection import SessionLambda, engine
 
@@ -86,7 +83,9 @@ async def create_costumer(costumer: CostumerBase, db: db_dependency):
         "teléfono": int(new_phone.phone)
     }
 
-# LISTAR TODOS LOS CLIENTES
+
+
+# LISTAR CLIENTES
 @app.get("/costumers/{phone}", status_code=status.HTTP_200_OK)
 async def get_costumers(phone: int, db: db_dependency):
     # BUSCAR EL USUARIO
@@ -104,6 +103,9 @@ async def get_costumers(phone: int, db: db_dependency):
         "teléfono": int(instance.phone)
     }
 
+
+
+# ACTUALIZAR EL CLIENTE
 @app.put("/costumers/{phone}", status_code=status.HTTP_202_ACCEPTED)
 async def update_costumer(phone: int, costumer: CostumerBase, db: db_dependency):
     # ACTUALIZAR EL USUARIO
@@ -132,7 +134,7 @@ async def update_costumer(phone: int, costumer: CostumerBase, db: db_dependency)
 
 
 
-
+# ELIMINAR EL CLIENTE
 @app.delete("/costumers/{phone}", status_code=status.HTTP_200_OK)
 async def delete_costumer(phone: int, db: db_dependency):
     # ELIMINAR EL USUARIO
