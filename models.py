8fb5fc4ff889aac45f7f55a0ccf4d5a589da5c5f
wolf <arthurwolf.dev07@gmail.com>
2024-07-31 20:34:06 -0600

    UPDATE phones & costumers tables, CheckConstraint age_between_18_and_100, phone as str

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Column
+from sqlalchemy import String, Integer, Column, CheckConstraint
 from connection import Base
 
 # TABLA DE CLIENTES CON DATOS PERSONALES
@@ -6,9 +6,9 @@ class Costumer(Base):
     __tablename__ = "costumers"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    last_name = Column(String)
-    age = Column(Integer)
+    name = Column(String(50))
+    last_name = Column(String(50))
+    age = Column(Integer, CheckConstraint('age >= 18 AND age <= 100', name='age_between_18_and_100'))
 
 # TABLA DE CLIENTES PARA TELÉFONOS
 class Phones(Base):
@@ -16,4 +16,4 @@ class Phones(Base):
 
     id = Column(Integer, primary_key=True)
     costumer_id = Column(Integer)
-    phone = Column(Integer, unique=True)
+    phone = Column(String(15), unique=True)
